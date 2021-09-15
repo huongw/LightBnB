1a84db2f2315e6bf1bcb55aa30ec4a004ce5438e
huongw <whitneyhuong@gmail.com>
2021-09-15 22:12:17 +0000

    insert all data into tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (name, email, password) VALUES ('Samantha', 'girlgamer2@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name, email, password) VALUES ('Christian', 'christian.smith89@yahoo.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name, email, password) VALUES ('Egor', 'e.gorgeous2@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name, email, password) VALUES ('Tina', 'popstar123@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+INSERT INTO users (name, email, password) VALUES ('Julian', 'julesgotgame34@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ('Big red', 'Description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 200, 2, 1, 2, 'Canada', '12 Elm Street', 'Vancouver', 'British Columbia', 'A2G 4N3');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ('Speed lamp', 'Description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 300, 2, 3, 3, 'Canada', '34 Sesame Street', 'Brampton', 'Ontario', 'N3X 9F0');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ('Habit mix', 'Description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 500, 3, 3, 3, 'USA', '78 Blue Cres', 'Vaughan', 'Ontario', 'n4S 2H4');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ('Fun glad', 'Description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 200, 1, 2, 2, 'Germany', '21 King Street', 'Etobicoke', 'Ontario', 'S3G 3k6');
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ('Shine twenty', 'Description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 400, 3, 2, 2, 'Russia', '241 Pie Cres', 'Markham', 'Ontario', 'J6F 2G7');
+
+INSERT INTO reservations (start_date, end_date) VALUES ('2018-09-11', '2018-09-26');
+INSERT INTO reservations (start_date, end_date) VALUES ('2019-01-04', '2019-02-01');
+INSERT INTO reservations (start_date, end_date) VALUES ('2019-05-11', '2019-05-13');
+INSERT INTO reservations (start_date, end_date) VALUES ('2019-09-12', '2021-09-14');
+INSERT INTO reservations (start_date, end_date) VALUES ('2019-10-01', '2021-10-05');
+INSERT INTO reservations (start_date, end_date) VALUES ('2020-05-01', '2021-05-05');
+
+INSERT INTO property_reviews (rating, message) VALUES (3, 'message');
+INSERT INTO property_reviews (rating, message) VALUES (2, 'message');
+INSERT INTO property_reviews (rating, message) VALUES (5, 'message');
+INSERT INTO property_reviews (rating, message) VALUES (4, 'message');
+INSERT INTO property_reviews (rating, message) VALUES (3, 'message');
+INSERT INTO property_reviews (rating, message) VALUES (1, 'message');
