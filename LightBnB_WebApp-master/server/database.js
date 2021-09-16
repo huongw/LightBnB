380e285415da37c85f3fb376c7ad4121b7527d2d
huongw <whitneyhuong@gmail.com>
2021-09-16 18:41:38 +0000

    update hash password and implement query for getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -73,7 +73,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.* FROM reservations
+          WHERE guest_id = $1
+          LIMIT $2`, 
+  [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    return err.message;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
